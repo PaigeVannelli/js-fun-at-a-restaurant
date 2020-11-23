31a3da2285d5d740d09d55f1f6fd24327ea69132
Paige Vannelli <paigevannelli@gmail.com>
2020-11-23 15:41:39 -0700

    Add restaurant object

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,25 @@
+// function createRestaurant(name) {
+//   //Input is a string and outpout is an object with a name
+//   //create an object with a porperty of name
+//   //you should ne able ot input any name
+//
+//   var pizzaRestaurant = {
+//     name: name
+// }
 
 
+function createRestaurant(name) {
+  var restaurant = {
+    name: name,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: [],
+    }
+  }
+}
 module.exports = {
-  // createRestaurant, 
+  createRestaurant,
   // addMenuItem,
   // removeMenuItem
 }
