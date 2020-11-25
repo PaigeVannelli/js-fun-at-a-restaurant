8e92cf07f6fbabf26e299f97875fdec048f69834
Paige Vannelli <paigevannelli@gmail.com>
2020-11-24 22:02:21 -0700

    Add answers to half of chef

--- src/Chef.js
+++ src/Chef.js
@@ -1,6 +1,16 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
+  }
+  greetCustomer(name, morning) {
+    if (morning === true) {
+      return 'Good morning, ' + name + '!'
+    }
+    return 'Hello, ' + name + '!'}
+
+  checkForFood(foodItem){
+    return 'Yes, we\'re serving ' + foodItem.name + ' today!'
   }
 }
 
