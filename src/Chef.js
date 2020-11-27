0a1c6ac65a7de401f8aee5318604e0878a1ae967
Paige Vannelli <paigevannelli@gmail.com>
2020-11-27 16:37:46 -0700

    Add sqiare bracket notation answers

--- src/Chef.js
+++ src/Chef.js
@@ -10,22 +10,37 @@ class Chef {
     return 'Hello, ' + name + '!'
   }
 
+  // checkForFood(foodItem){
+  //   if (foodItem.type === 'breakfast' && this.restaurant.menus.breakfast.includes(foodItem)) {
+  //     return 'Yes, we\'re serving ' + foodItem.name + ' today!'
+  //   } else if (foodItem.type === 'lunch' && this.restaurant.menus.lunch.includes(foodItem)) {
+  //     return 'Yes, we\'re serving ' + foodItem.name + ' today!'
+  //   } else if (foodItem.type === 'dinner' && this.restaurant.menus.dinner.includes(foodItem)) {
+  //     return 'Yes, we\'re serving ' + foodItem.name + ' today!'
+  //   } else {
+  //     return 'Sorry, we aren\'t serving ' + foodItem.name + ' today.'
+  //   }
+  // }
+
+  // checkForFood(foodItem){
+  //   if (foodItem['type'] === 'breakfast' && this.restaurant['menus']['breakfast'].includes(foodItem)) {
+  //     return 'Yes, we\'re serving ' + foodItem.name + ' today!'
+  //   } else if (foodItem['type'] === 'lunch' && this.restaurant['menus']['lunch'].includes(foodItem)) {
+  //     return 'Yes, we\'re serving ' + foodItem.name + ' today!'
+  //   } else if (foodItem['type'] === 'dinner' && this.restaurant['menus']['dinner'].includes(foodItem)) {
+  //     return 'Yes, we\'re serving ' + foodItem.name + ' today!'
+  //   } else {
+  //     return 'Sorry, we aren\'t serving ' + foodItem.name + ' today.'
+  //   }
+  // }
+
   checkForFood(foodItem){
-    if (foodItem.type === 'breakfast' && this.restaurant.menus.breakfast.includes(foodItem)) {
-      return 'Yes, we\'re serving ' + foodItem.name + ' today!'
-    } else if (foodItem.type === 'lunch' && this.restaurant.menus.lunch.includes(foodItem)) {
-      return 'Yes, we\'re serving ' + foodItem.name + ' today!'
-    } else if (foodItem.type === 'dinner' && this.restaurant.menus.dinner.includes(foodItem)) {
+    if (this.restaurant['menus'][foodItem.type].includes(foodItem)) {
       return 'Yes, we\'re serving ' + foodItem.name + ' today!'
     } else {
       return 'Sorry, we aren\'t serving ' + foodItem.name + ' today.'
     }
   }
 }
-
-//should be an if else {
-  //if foodItem is on the menu - check properties of menu?
-
-
 
 module.exports = Chef;
